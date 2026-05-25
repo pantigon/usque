a6e9bca79c5c7f6b3b3c8d672748e5b98a308160
pantigon <47517210+pantigon@users.noreply.github.com>
2026-05-25 21:56:15 +0700

    Update import paths to new GitHub repository

--- cmd/socks.go
+++ cmd/socks.go
@@ -8,9 +8,9 @@ import (
 	"os"
 	"time"
 
-	"github.com/Diniboy1123/usque/api"
-	"github.com/Diniboy1123/usque/config"
-	"github.com/Diniboy1123/usque/internal"
+	"github.com/pantigon/usque/api"
+	"github.com/pantigon/usque/config"
+	"github.com/pantigon/usque/internal"
 	"github.com/spf13/cobra"
 	"golang.zx2c4.com/wireguard/tun/netstack"
 )
